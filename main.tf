e6888499e3a345d323186847ca2322fe09fd0a2e
vaibhav-infracloud <vaibhav.gupta@infracloud.io>
2022-12-22 11:53:49 +0530

    Importing all users from AWS SSO into state file

--- main.tf
+++ main.tf
@@ -1,23 +1,20 @@
 
 resource "aws_identitystore_user" "example" {
-  for_each          = var.users
+  for_each          = {for u in var.users:  u.UserName => u}
   identity_store_id = tolist(data.aws_ssoadmin_instances.example.identity_store_ids)[0]
-
-  #display_name = format("%s %s", title(split(".", split("@", each.value)[0])[0]), title(split(".", split("@", each.value)[0])[1]))
-  display_name = each.value["display_name"]
-  user_name    = each.value["user_name"]
+  
+  display_name = each.value.DisplayName
+  user_name    = each.value.UserName
 
   name {
-    #given_name  = title(split(".", split("@", each.value)[0])[0])
-    #family_name = title(split(".", split("@", each.value)[0])[1])
-    given_name  = each.value["given_name"]
-    family_name = each.value["family_name"]
+    given_name  = each.value.GivenName
+    family_name = each.value.FamilyName
   }
 
   emails {
-    value   = each.value["user_name"]
-    primary = true
-    type    = "work"
+    value= each.value.UserName
+    primary = each.value.Primary
+    type = each.value.Type
   }
 }
 resource "aws_identitystore_group" "this" {
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "groups" {
 }
 
-#variable "users" {
-#}
-
 variable "users" {
-  type = map(object({
-    display_name = string
-    given_name   = string
-    family_name  = string
-    user_name    = string
-  }))
 }
+
+#variable "users" {
+#  type = map(object({
+#    display_name = string
+#    given_name   = string
+#    family_name  = string
+#    user_name    = string
+#  }))
+#}
 
 variable "user_in_group" {
   type = list(object({
