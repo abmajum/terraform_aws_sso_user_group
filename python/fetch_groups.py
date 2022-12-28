66dabb49e0f7e88222a9b279849dfc4ad6f1ec9b
vaibhav-infracloud <vaibhav.gupta@infracloud.io>
2022-12-28 10:49:14 +0530

    Adding comments in existing python scripts

--- python/fetch_groups.py
+++ python/fetch_groups.py
@@ -5,13 +5,18 @@ client = boto3.client('identitystore')
 IdentityStoreId="d-9f672ca174"
 group_list=[]
 
+#Initialising an empty group_map that will contain vaules for a group
 group_map={}
 
+#fetching list of all the groups using list_users function
 response = client.list_groups( IdentityStoreId=IdentityStoreId)
 for group in response['Groups']:
     print("terraform import \'aws_identitystore_group.this[\"",group['DisplayName'],"\"]\' ", IdentityStoreId ,"/",group['GroupId'], sep='')
+    #Creating a group map for each group to be used in terraform.vars
     group_map["DisplayName"] = group["DisplayName"]
     group_map["Description"]=group.get('Description')
+    #Appending the copy of the group_map to the group list
     group_list.append(copy.deepcopy(group_map))
 
 print(json.dumps(group_list, indent=4))
+
--- python/fetch_users.py
+++ python/fetch_users.py
@@ -5,18 +5,24 @@ client = boto3.client('identitystore')
 IdentityStoreId="d-9f672ca174"
 user_list=[]
 
+#Initialising an empty user_map that will contain vaules for a user
 user_map={}
 
+#fetching list of all the users using list_users function
 response = client.list_users( IdentityStoreId=IdentityStoreId)
 for user in response['Users']:
     print("terraform import \'aws_identitystore_user.example[\"",user['UserName'],"\"]\'", IdentityStoreId ,"/",user['UserId'], sep='')
     print (user)
+    #Creating a user map for each user to be used in terraform.vars
     user_map["UserName"] = user["UserName"]
     user_map["DisplayName"] = user["DisplayName"]
     user_map["FamilyName"]=user['Name']["FamilyName"]
     user_map["GivenName"]=user['Name']["GivenName"]
     user_map["Primary"] =user["Emails"][0].get("Primary")
     user_map["Type"]= user["Emails"][0].get("Type")
+    #Appending the copy of the user_map to the user list
     user_list.append(copy.deepcopy(user_map))
 
+
 print(json.dumps(user_list, indent=4))
+
